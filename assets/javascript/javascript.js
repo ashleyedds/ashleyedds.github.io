6de4c88f124a63235989bf92c0daa83acfb7af05
Ashley Edds <ashley@edds.net>
2018-01-21 10:18:28 -0500

    redesign, transparent nav, blur hover

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,80 @@
+  
+//   NAVBAR
+
+$(document).ready(function(){
+    $(".navbar-default").css("background", "transparent");
+  });
+
+  $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
+            $(".navbar-default").css("background", "#443e41");
+        } else {
+            $(".navbar-default").css("background", "transparent");
+        }
+    });
+
+    // IMG EFFECTS
+    $(".overlayAmsterdam").mouseover(function() {
+        $(".selfieAmsterdam").css("filter", "blur(2px)");
+        $(".overlayAmsterdam").css("opacity", "1");
+    })
+    $(".overlayAmsterdam").mouseout(function() {
+        $(".selfieAmsterdam").css("filter", "blur(0px)");
+        $(".overlayAmsterdam").css("opacity", "0");
+    })
+
+
+    $(".overlayCharlie").mouseover(function() {
+        $(".selfieCharlie").css("filter", "blur(2px)");
+        $(".overlayCharlie").css("opacity", "1");
+    })
+    $(".overlayCharlie").mouseout(function() {
+        $(".selfieCharlie").css("filter", "blur(0px)");
+        $(".overlayCharlie").css("opacity", "0");
+    })
+
+
+    $(".overlayRome").mouseover(function() {
+        $(".selfieRome").css("filter", "blur(2px)");
+        $(".overlayRome").css("opacity", "1");
+    })
+    $(".overlayRome").mouseout(function() {
+        $(".selfieRome").css("filter", "blur(0px)");
+        $(".overlayRome").css("opacity", "0");
+    })
+
+
+    $(".overlaySelfie").mouseover(function() {
+        $(".selfieSelfie").css("filter", "blur(2px)");
+        $(".overlaySelfie").css("opacity", "1");
+    })
+    $(".overlaySelfie").mouseout(function() {
+        $(".selfieSelfie").css("filter", "blur(0px)");
+        $(".overlaySelfie").css("opacity", "0");
+    })
+
+
+    $(".overlayFrance").mouseover(function() {
+        $(".selfieFrance").css("filter", "blur(2px)");
+        $(".overlayFrance").css("opacity", "1");
+    })
+    $(".overlayFrance").mouseout(function() {
+        $(".selfieFrance").css("filter", "blur(0px)");
+        $(".overlayFrance").css("opacity", "0");
+    })
+
+    $(".overlayUNC").mouseover(function() {
+        $(".selfieUNC").css("filter", "blur(2px)");
+        $(".overlayUNC").css("opacity", "1");
+    })
+    $(".overlayUNC").mouseout(function() {
+        $(".selfieUNC").css("filter", "blur(0px)");
+        $(".overlayUNC").css("opacity", "0");
+    })
+
+
+    
+
+    
+
+
