f7a3b34a2d995145ea4407631df54e4be37a25cf
Ashley Edds <ashley@edds.net>
2018-02-19 21:50:55 -0500

    contact section, footer added

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,16 +1,20 @@
-  
-$(document).ready(function(){
+
+$(document).ready(function () {
     $("nav").css("background", "transparent");
-  });
+});
 
-  $(window).scroll(function () {
-        if ($(window).scrollTop() > 50) {
-            $("nav").css("background", "#443e41");
-        } else {
-            $("nav").css("background", "transparent");
-        }
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 50) {
+        $("nav").css("background", "#443e41");
+    } else {
+        $("nav").css("background", "transparent");
+    }
 
-$(".button-collapse").sideNav();
+    $(".button-collapse").sideNav();
 
-$('.carousel').carousel();
-    });
+    $('.carousel').carousel();
+
+    $('#textarea1').val('');
+    $('#textarea1').trigger('autoresize');
+
+});
